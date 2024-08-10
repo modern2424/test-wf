284ad6ba33b9570f9af8759e7df9c315e0893954
akshith-git <50228497+akshith-git@users.noreply.github.com>
2024-08-10 18:46:23 -0500

    Update copy-update-secrets-repo.sh

--- scripts/copy-update-secrets-repo.sh
+++ scripts/copy-update-secrets-repo.sh
@@ -5,12 +5,13 @@
 #ORG_NAME=""
 #SECRET_NAME=""
 #SECRET_VALUE=""
+#REPO_NAME=""
 
 # Step 1: Get the public key for the organization
 echo "Fetching the public key for the organization..."
 response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
                 -H "Accept: application/vnd.github+json" \
-                https://api.github.com/orgs/$ORG_NAME/actions/secrets/public-key)
+                https://api.github.com/$ORG_NAME/$REPO_NAME/actions/secrets/public-key)
 
 key_id=$(echo "$response" | jq -r .key_id)
 public_key=$(echo "$response" | jq -r .key)
@@ -44,11 +45,11 @@ fi
 
 echo "Secret encrypted."
 # Step 3: Create or update the organization secret
-echo "Creating or updating the organization secret..."
+echo "Creating or updating the repository secret..."
 update_response=$(curl -s -X PUT -H "Authorization: Bearer $GITHUB_TOKEN" \
                      -H "Accept: application/vnd.github+json" \
                      -d "{\"encrypted_value\":\"$encrypted_value\", \"key_id\":\"$key_id\", \"visibility\":\"private\"}" \
-                     https://api.github.com/orgs/$ORG_NAME/actions/secrets/$SECRET_NAME)
+                     https://api.github.com/$ORG_NAME/$REPO_NAME/actions/secrets/$SECRET_NAME)
 
 #if echo "$update_response" | grep -q '"created_at"'; then
 if [[ "$update_response" == "201" || "$update_response" == "204" ]]; then
